2619c4e4e06da03fe421792e9f01303916dd291f
xacmoto <cavestory@protonmail.com>
2025-12-04 23:16:37 -0600

    LOL

---
+++ assets/scripts/game.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# if random float between 1 and 0 is greater
+func oppurtunity_check(weight: float) -> bool:
+	
+	var success: bool = false
+	var chance: float = randf()
+	
+	if chance >= weight:
+		success = true
+		
+	return success
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
